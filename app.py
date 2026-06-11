f797bbe7a85dd817bd0c5b068b95efa2440c6bd0
abdoulrahmanebande <abdoulrahmanebande@gmail.com>
2026-06-11 16:46:16 +0530

    Backend api

--- app.py
+++
@@ -1,8 +0,0 @@
-from src.data_ingestion_pipeline.data_loader import load_all_documents
-from src.retrieval_pipeline.rag_search import RAGSearch
-
-if __name__ == '__main__':
-  rag_search = RAGSearch()
-  
-  summary = rag_search.search_and_summarize(query='Qui est Abdoul-Rahmane BANDE ?',top_k=3)
-  print(f"Here is the summary:\n{summary}")
